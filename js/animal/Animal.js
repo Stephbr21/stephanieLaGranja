06515794382617e43eeb2b5f26023d29fceb9607
Stephanie <stephanie.barrantes21@gmail.com>
2017-10-26 19:09:55 -0600

    AddFolders

---
+++ js/animal/Animal.js
@@ -0,0 +1,42 @@
+var Animal = (
+	function () {
+		function Animal(pNombre) {
+
+        }
+
+        function Animal(pedad) {
+            
+        }
+
+        function Animal(paltura) {
+            
+        }
+
+        function Animal(ppeso) {
+            
+        }
+
+        function Animal(ptamanno) {
+            
+        }
+            
+        function Animal(pEdad) {
+                        
+        }
+            
+        function Animal(pcapacidadEstomago) {
+                        
+        }
+            
+        function Animal(pPeso) {
+                        
+        }
+
+		//Class Methods
+		Animal.prototype.comer = function () {
+			console.log(this.nombre + ' soy una vaca y estoy comiendo.');
+		}
+
+		return VAnimalaca;
+	}
+)();
