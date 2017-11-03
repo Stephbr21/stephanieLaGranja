5c7b5d0c0dd54f45ce8a6007a3dff10b5602cc6c
Stephanie <stephanie.barrantes21@gmail.com>
2017-11-02 20:03:30 -0600

    changes2

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ window.addEventListener('load', init, false);
 
 function init() {
 
-    var matilde = new Vaca('Matilde');
-    matilde.comer();
+    // var matilde = new Vaca('Matilde');
+    // matilde.comer();
 
-    var juana = new Vaca('Juana');
-    juana.comer();
-    //juana.comerPapa();
+    // var juana = new Vaca('Juana');
+    // juana.comer();
+    // //juana.comerPapa();
 
-    console.log(matilde);
+    // console.log(matilde);
+
+    var animalsContainer = document.getElementById('animalsContainer');
+    //console.log(animalsContainer);
+
+    for (var index = 0; index < 5; index++) {
+        var animal = document.createElement('div');
+        //console.log(animal);
+        animalsContainer.appendChild(animal);
+        animal.id = 'animal'+ index;
+        animal.style.with = '100px';
+        animal.style.height = '100px';
+        animal.style.background = 'red';
+        animal.addEventListener('click', onClick,false);
+    
+            function onClick(e){
+                console.log(e.target.id);
+            }
+        
+    }
+    
+        
+
 }
