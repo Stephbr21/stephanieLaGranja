837649248667e6be8d14519176582edd01320be4
Stephanie <stephanie.barrantes21@gmail.com>
2017-11-05 14:10:18 -0600

    AddCss

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
 window.addEventListener('load', init, false);
 
 function init() {
-
-    var cusu = new Animal('Batido', 1, 2, 10, 'mediana', 10, 5, 5, 'ninguna', 'ninguno');
-    //cusu.comer();
-    //cusu.caminar();
-    //cusu.beber();
+    // var cusu = new Animal('Batido', 1, 2, 10, 'mediana', 10, 5, 5, 'ninguna', 'ninguno');
+    // //cusu.comer();
+    // //cusu.caminar();
+    // //cusu.beber();
 
     var animals = [];
     var vaca = new Vaca('Matilde');
     vaca.color = 'blue';
     animals.push(vaca);
 
+    var gato = new Gato('Minino');
+    vaca.color = 'purple';
+    animals.push(gato);
 
+    var perro = new Perro('Booby');
+    vaca.color = 'green';
+    animals.push(perro);
+    
+    var caballo = new Caballo('Clotilde');
+    vaca.color = 'pink';
+    animals.push(caballo);
+
+    var gallina = new Gallina('Plumin');
+    vaca.color = 'black';
+    animals.push(gallina);
+
+    var gallina = new Gallina('Blanca');
+    vaca.color = 'blue';
+    animals.push(gallina);
+
+    var pato = new Pato('Poppy');
+    vaca.color = 'cyan';
+    animals.push(pato);
+
+    var pato = new Pato('Pipo');
+    vaca.color = 'blue';
+    animals.push(pato);
+
+    var cerdo = new Cerdo('Jonas');
+    vaca.color = 'red';
+    animals.push(cerdo);
+    
 
     console.log(animals);
 
     var animalContainer_title = document.getElementById('animalContainer_title');
-    var animalContainer_title = document.getElementById('farmContainer_tittle');
-    var animalContainer_title = document.getElementById('farmContainer_money');
-
+    var farmContainer_money = document.getElementById('farmContainer_money');
+    var farmContainer_egg = document.getElementById('farmContainer_egg');
+    var farmConainer_milk = document.getElementById('farmConainer_milk');
+    var farmContainer_bacon = document.getElementById('farmContainer_bacon');
+    var farmContainer_corn = document.getElementById('farmContainer_corn');
+    var farmContainer_food = document.getElementById('farmContainer_food');
+    var farmContainer_grass = document.getElementById('farmContainer_grass');
+    
     /*
     let canvas = document.createElement('canvas');
     document.body.appendChild(canvas);
